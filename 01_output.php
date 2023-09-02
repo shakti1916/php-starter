162c105b181a16d3f6f74a9b9bef951383834af2
shakti1916 <shaktimaddeshiya34321@gmail.com>
2023-09-02 16:58:33 +0530

    output print in php

--- 01_output.php
+++ 01_output.php
@@ -1,4 +1,33 @@
 <?php
-echo 'Hello'
+// print no string 
+echo 123,'Hello', 10,5;
+
+// use keyword print 
+
+print 123;
+
+// print array
+print_r([1,2,3]);
+
+// it give the data type 
+
+var_dump("Hello");
+var_dump(true);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?=  'hi this is shakti'?>
+    </h1>
+    
+</body>
+</html>
+
